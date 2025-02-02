d08a2c2fc20a8fa0cee198be3613dde00ad5dc22
Andreas Welker <andreaswelker@aol.com>
2025-02-02 23:28:44 +0100

    make contact input

--- src/app/main/main-contact/main-contact.component.ts
+++ src/app/main/main-contact/main-contact.component.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-main-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './main-contact.component.html',
   styleUrl: './main-contact.component.scss'
 })
 export class MainContactComponent {
 scrolltotop(){
   window.scrollTo({top:0,behavior:"smooth"})
+}
+
+http = inject(HttpClient);
+
+
+contactDate ={
+  name:"",
+  mail:"",
+  massage:"",
+  };
+
+    mailTest = true;
+
+  post = {
+    endPoint: 'https://andreaswelker.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  // onSubmit(ngForm: NgForm) {
+  //   if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+     
+  //   } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+  //     ngForm.resetForm();
+  //   }
+  // }
+
+
+onSubmit(ngForm: NgForm) {
+if(ngForm.valid && ngForm.submitted){
+  this.http.post(this.post.endPoint, this.post.body(this.contactDate))
+  .subscribe({
+    next: (response) => {
+
+      ngForm.resetForm();
+    },
+    error: (error) => {
+      console.error(error);
+    },
+    complete: () => console.info('send post complete'),
+  });
+}
 }
 }
--- src/app/main/main-projects/main-projects.component.ts
+++ src/app/main/main-projects/main-projects.component.ts
@@ -8,6 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './main-projects.component.scss'
 })
 export class MainProjectsComponent {
+  projects:{
+    name: string;
+    description: string;
+    technologies: string;
+    image: string;
+    URL: string;
+  }[] =[
+    {name: "EL Pollo Loco",
+      description: "es war einmal",
+      technologies: "./",
+      image: "./",
+      URL: "url"
+    },
+    {name: "Pokedex",
+      description: "es war einmal",
+      technologies: "./",
+      image: "./",
+      URL: "url"
+    },
+    {name: "test",
+      description: "es war einmal",
+      technologies: "./",
+      image: "./",
+      URL: "url"
+    }
+  ]
+
+  
   scrollToBottom() {
     window.scrollTo(0, (window.document.body.scrollHeight - window.innerHeight));
   }
